cdc9ccdd3ceb210b874aa6db13455e42064fa239
macarong <44965898+nengmyun@users.noreply.github.com>
2024-04-23 23:59:14 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '엔더 너드',
+  tagline: '언리미티드 어드벤쳐, 언리미티드 백팩 등 엔더 너드들의 창의적인 상품들을 한국어로 더욱 편하게 입문하세요!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
