44ac03229bc358d43c8ac5037425c18c8cbfa617
macarong <44965898+nengmyun@users.noreply.github.com>
2024-08-30 15:28:50 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="docs/intro/">
+            to="/docs/intro">
             시작하기!
           </Link>
         </div>
